e724239847ff9c01a10a25db11b8b2465fea75f7
jonas-45 <jonaswullah@gmail.com>
2023-04-14 22:13:30 +0000

    Change double quotes to single quotes

--- morsecode.rb
+++ morsecode.rb
@@ -1,17 +1,17 @@
 MORSE = {
-    ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E",
-    "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J",
-    "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",
-    ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
-    "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
-    "--.." => "Z"
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
 }
 
 def decode_char(code)
   MORSE[code] || ''
 end
 
-puts decode_char(".-")
+puts decode_char('.-')
 
 
 def decode_word(word_code)
@@ -19,7 +19,7 @@ def decode_word(word_code)
   code_split.map {|char| MORSE[char]}.join
  end
  
- puts decode_word("-- -.--")
+ puts decode_word('-- -.--')
 
  def decode(msg_code)
   message_split = msg_code.split('  ')
@@ -28,5 +28,5 @@ def decode_word(word_code)
   end.join(' ')
 end
 
-puts decode("-- -.--   -. .- -- .")
+puts decode('-- -.--   -. .- -- .')
  
